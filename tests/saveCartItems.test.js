10221dd4f46f91a349eb386ef69fef90c89f811b
Ivens <ivensarroxellas@hotmail.com>
2022-07-14 14:48:32 -0300

    req 6

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -4,6 +4,18 @@ const saveCartItems = require('../helpers/saveCartItems');
 localStorageSimulator('setItem');
 
 describe('3 - Teste a função saveCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Testa função chamando parametro "<ol><li>Item</li></ol>" se ela é chamada', async () => {
+    expect.assertions(1);
+
+    saveCartItems('<ol><li>Item</li></ol>');
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+  });
+  
+  it('Testa função chamando parametro "<ol><li>Item</li></ol>" se ela é chamado com 2 parâmetros, primeiro sendo o de carItems e segundo parametro de saveCartItems', async () => {
+    expect.assertions(1);
+
+    saveCartItems('<ol><li>Item</li></ol>');
+    expect(localStorage.setItem).toHaveBeenCalledWith('cartItems','<ol><li>Item</li></ol>');
+  });
+
 });
